a14bb80b0e9fed50c2d703185771ebeaf2d4a56e
clement <clement.saunier@epitech.eu>
2022-07-05 12:02:23 +0200

    fix number generation js vs. sol

--- contracts/Generator.sol
+++ contracts/Generator.sol
@@ -15,27 +15,6 @@ contract Generator {
 
     constructor() {}
 
-    function getNumber(uint lgBytes) public pure returns (uint) {
-        return lgBytes;
-    }
-
-    function bytes32ToString(bytes32 x) public pure returns (string memory) {
-        bytes memory bytesString = new bytes(32);
-        uint charCount = 0;
-        for (uint j = 0; j < 32; j++) {
-            bytes1 char = bytes1(bytes32(uint(x) * 2**(8 * j)));
-            if (char != 0) {
-                bytesString[charCount] = char;
-                charCount++;
-            }
-        }
-        bytes memory bytesStringTrimmed = new bytes(charCount);
-        for (uint j = 0; j < charCount; j++) {
-            bytesStringTrimmed[j] = bytesString[j];
-        }
-        return string(bytesStringTrimmed);
-    }
-
     function read(
         bytes32 seed,
         uint256 index,
@@ -59,12 +38,12 @@ contract Generator {
         return (rand.seed);
     }
 
-    function getFirstBytes(address key, uint16[] memory genes)
+    function getBytes(address key, uint16[] memory genes, uint16 count)
         public
         pure
         returns (bytes memory)
     {
-        return read(getSeed(key, genes), 0, 1);
+        return read(getSeed(key, genes), 0, count);
     }
 
     function bytesToUint(bytes memory b) internal pure returns (uint256) {
@@ -78,18 +57,13 @@ contract Generator {
         return number;
     }
 
-    function getFirstNumber(address key, uint16[] memory genes)
+    function getNumber(address key, uint16[] memory genes, uint16 count)
         public
         pure
         returns (uint256)
     {
         bytes32 seed = getSeed(key, genes);
-        bytes memory nb = read(seed, 0, 1);
-        uint16 x;
-
-        assembly {
-            x := mload(add(nb, 1))
-        }
+        bytes memory nb = read(seed, 0, count);
 
         return (bytesToUint(nb));
     }
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,27 +1,40 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const { getHash} = require("../scripts/random");
+const { getHash } = require("../scripts/random");
 
-describe("Random Generation", function () {
-
+describe("Random Generation", function() {
   let instance;
 
-  beforeEach(async () => { 
+  beforeEach(async () => {
     const contract = await ethers.getContractFactory("Generator");
     instance = await contract.deploy();
-  })
+  });
 
-  it("Should return a number", async function () {
+  it("Should return a number", async function() {
     const [owner, user] = await ethers.getSigners();
+    const byteCount = 1;
+    const jsBytes = getHash(owner.address).substring(2, 2 + 2 * byteCount);
+    const solBytes = await instance.getBytes(
+      owner.address,
+      [0, 0, 0, 0],
+      byteCount
+    );
 
-    console.log('from:', owner.address);
+    console.log("from:", owner.address);
     console.log("sol :", await instance.getSeed(owner.address, [0, 0, 0, 0]));
     console.log("js  :", getHash(owner.address));
 
-    console.log("1 byte sol:", await instance.getFirstBytes(owner.address, [0, 0, 0, 0]));
+    console.log("bytes:", solBytes, "vs", jsBytes);
 
-    console.log("1st nb sol :", await instance.getFirstNumber(owner.address, [0, 0, 0, 0]));
-    console.log("1st nb sol :", parseInt(getHash(owner.address).substring(2, 4), 16));
-
+    console.log(
+      byteCount,
+      "b nb (sol) :",
+      (await instance.getNumber(
+        owner.address,
+        [0, 0, 0, 0],
+        byteCount
+      )).toNumber()
+    );
+    console.log(byteCount, "b nb (js)  :", parseInt(jsBytes, 16));
   });
 });
