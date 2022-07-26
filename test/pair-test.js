6e98cb0f2f05d7f27f7e128ed98593335727d310
clement <clement.saunier@epitech.eu>
2022-07-26 15:14:57 +0200

    optimized test for large numbers

--- test/pair-test.js
+++ test/pair-test.js
@@ -10,6 +10,31 @@ describe("Generators pair testing", function() {
   let gInstance;
   let instance;
 
+  // const issueBytes = async (address, genes, iterations) => {
+  //   const rand = new Generator(address, genes);
+  //   const contract = instance;
+
+  //   const js = [...Array(iterations)].map((_, i) => rand.popUInt8());
+  //   const sol = await contract.dumpUInt8(address, genes, iterations);
+
+  //   return { js, sol };
+  // };
+
+  const issueBytes = async (address, genes, iterations, bytes = 1) => {
+    const rand = new Generator(address, genes);
+    const contract = instance;
+    const solByteValues = [
+      () => contract.dumpUInt8(address, genes, iterations),
+      () => contract.dumpUInt16(address, genes, iterations)
+    ];
+    const jsByteValues = [() => rand.popUInt8(), () => rand.popUInt16()];
+
+    const js = [...Array(iterations)].map(jsByteValues[bytes - 1]);
+    const sol = await solByteValues[bytes - 1]();
+
+    return { js, sol };
+  };
+
   beforeEach(async () => {
     const contract = await ethers.getContractFactory("Generator");
     gInstance = await contract.deploy();
@@ -30,133 +55,88 @@ describe("Generators pair testing", function() {
     });
   });
 
-  it("Matches mutation distribution on 1 byte", async function() {
-    const [owner, user] = await ethers.getSigners();
-    const count = 32;
-    const mutation = 50;
-    const geneCount = 6;
-    const genome = i => i === -1 ? [...Array(geneCount)].fill(0) : [...Array(geneCount)].fill(0).fill(mutation, i, i + 1);
+  describe("Gene influence", async function() {
+    const matchForNBytes = async function(bytes, count = 32 / bytes, log = true) {
+      const [owner] = await ethers.getSigners();
+      const address = owner.address;
+      const mutation = 50;
+      const geneCount = 6;
+      const genome = i =>
+        i === -1
+          ? [...Array(geneCount)].fill(0)
+          : [...Array(geneCount)].fill(0).fill(mutation, i, i + 1);
 
-    // [...Array(geneCount)].forEach((_, i) => {
-    //   console.log(genome(i));
-    // });
+      const original = await issueBytes(address, genome(-1), count, bytes);
+      const format = v => digits(bytes * 2 + 1, v.toString());
 
-    const bytes = i => {
-      const rnd = new Generator(owner.address, genome(i));
-      return [...Array(count)].map(() => digits(3, rnd.popUInt8().toString()));
-    };
+      log && console.log(`gene influence 1 byte (${mutation})`);
+      log &&console.log("bytes  :", ...original.js.map(v => format(v)));
 
-    const solBytes = async i => {
-      return (await instance.dumpUInt8(
-        owner.address,
-        genome(i),
-        count
-      )).map((v) => digits(3, v.toString()));
-    };
-
-    const fullSolBytes = await Promise.all(
-      [...Array(geneCount)].map(async (_, i) => solBytes(i))
-    );
-
-    const original = bytes(-1);
-
-    console.log(`mutation distribution 1 byte (${mutation})`);
-    console.log("bytes  :", ...(await solBytes(-1)));
-    console.log("bytes  :", ...original);
-
-    [...Array(geneCount)].forEach((byte, i) => {
-      const values = bytes(i);
-      const solValues = solBytes(i);
-      const displayed = bytes(i).map(
-        (v, i) => (v === original[i] ? chalk.red(v) : chalk.green(v))
+      let errorCount = 0;
+      await Promise.all(
+        [...Array(geneCount)].map(async (byte, i) => {
+          const values = await issueBytes(address, genome(i), count, bytes);
+          const displayed = values.js.map((v, i) => {
+            if (v != values.sol[i]) {
+              errorCount++;
+              return chalk.bgRed(format(v));
+            } else {
+              return v === original.js[i]
+                ? chalk.red(format(v))
+                : chalk.green(format(v));
+            }
+          });
+          if (!log) return;
+          console.log("gene", i, ":", ...displayed);
+        })
       );
-      console.log("gene", i, ":", ...displayed);
+      expect(errorCount).to.equal(
+        0,
+        "There were mismatches between JS and SOL values"
+      );
+    };
+    it("Display gene influence on 1 byte", async () => matchForNBytes(1));
+    it("Matches gene influence on 1 byte", async () => matchForNBytes(2, 500, false));
+    it("Matches gene influence on 2 byte", async () => matchForNBytes(2, 500, false));
+    it("Has fair value occurence", async function() {
+      const [owner, user] = await ethers.getSigners();
+      const count = 100000;
+      const values = [0, 1, 128, 254, 255];
+      const occurences = Array(values.length).fill(0);
+
+      const rnd = new Generator(owner.address, [0, 0, 0, 0]);
+
+      for (let i = 0; i < count; i++) {
+        const n = rnd.popUInt8();
+        values.forEach((value, idx) => {
+          if (value === n) occurences[idx] = occurences[idx] + 1;
+        });
+      }
+      occurences.forEach((value, i) => {
+        console.log(values[i], ":", value);
+      });
     });
-    [...Array(geneCount)].forEach((byte, i) => {
-      const values = bytes(i);
-      values.forEach((v, idx) => expect(v).to.equal(fullSolBytes[i][idx]));
-    })
-  });
 
-  it("Matches mutation distribution on 2 bytes", async function() {
-    const [owner, user] = await ethers.getSigners();
-    const count = 21;
-    const mutation = 50;
-    const geneCount = 6;
-    const genome = i => i === -1 ? [...Array(geneCount)].fill(0) : [...Array(geneCount)].fill(0).fill(mutation, i, i + 1);
-
-    const bytes = i => {
-      const rnd = new Generator(owner.address, genome(i));
-      return [...Array(count)].map(() => digits(5, rnd.popUInt16().toString()));
-    };
-
-    const solBytes = async i => {
-      return (await instance.dumpUInt16(
+    it("Generates int with min/max", async function() {
+      const [owner, user] = await ethers.getSigners();
+      const count = 32;
+      const genes = [0, 0, 0, 0];
+      const min = 0;
+      const max = 10;
+      const rnd = new Generator(owner.address, genes);
+      const jsValues = [...Array(count)].map(() => rnd.popInt(min, max, false));
+      const solValues = await instance.dumpInt(
         owner.address,
-        genome(i),
+        genes,
+        min,
+        max,
         count
-      )).map((v) => digits(5, v.toString()));
-    };
-
-    const fullSolBytes = await Promise.all(
-      [...Array(geneCount)].map(async (_, i) => solBytes(i))
-    );
-
-    const original = bytes(-1);
-
-    console.log(`mutation distribution (${mutation})`);
-    console.log("bytes  :", ...(await solBytes(-1)));
-    console.log("bytes  :", ...original);
-
-    await Promise.all([...Array(geneCount)].map(async (_, i) => {
-      const displayed = bytes(i).map(
-        (v, i) => (v === original[i] ? chalk.red(v) : chalk.green(v))
       );
-      console.log("gene", i, ":", ...displayed);
-    }));
 
-    [...Array(geneCount)].forEach(async (_, i) => {
-      const values = bytes(i);
-      values.forEach((v, idx) => expect(v).to.equal(fullSolBytes[i][idx]));
+      console.log("rbg for gene mutation:");
+      console.log("sol :", ...solValues);
+      console.log("js  :", ...jsValues);
+      jsValues.map((v, i) => expect(v).to.equal(solValues[i]));
     });
   });
-
-  it("Has fair value occurence", async function () {
-    const [owner, user] = await ethers.getSigners();
-    const count = 100000;
-    const values = [0, 1, 128, 254, 255];
-    const occurences = Array(values.length).fill(0);
-
-    const rnd = new Generator(owner.address, [0, 0, 0, 0]);
-
-    for (let i = 0; i < count; i++) {
-      const n = rnd.popUInt8();
-      values.forEach((value, idx) => {
-        if (value === n)
-          occurences[idx] = occurences[idx]  + 1;
-      })
-    }
-    occurences.forEach((value, i) => {
-      console.log(values[i], ":", value);
-    })
-  })
-
-  it("Generates int with min/max", async function () {
-    const [owner, user] = await ethers.getSigners();
-    const count = 32;
-    const genes = [0, 0, 0, 0];
-    const min = 0;
-    const max = 10;
-    const rnd = new Generator(owner.address, genes);
-    const jsValues = [...Array(count)].map(() => rnd.popInt(min, max, false));
-    const solValues = await instance.dumpInt(owner.address, genes, min, max, count);
-
-    console.log("rbg for gene mutation:");
-    console.log(
-      "sol :",
-      ...solValues
-    );
-    console.log("js  :", ...jsValues);
-    jsValues.map((v, i) => expect(v).to.equal(solValues[i]))
-  })
 });
